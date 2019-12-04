9fd6b8b516ce37fe44a02d46b52b47619eda684e
Vicky <ying.vicky.cao@outlook.com>
2019-12-04 22:08:07 +0800

    Python: list sort, reverse

--- src/1_data_type/test_list.py
+++ src/1_data_type/test_list.py
@@ -10,13 +10,12 @@ def access_list_item():
 
     # -2 = 倒数第二个
     print(phone_device_types[-2])
-
+    
     # print(phone_device_types[-3])                 # ERROR: Traceback (most recent call last): IndexError: list index out of range
 
     shopping_name = []
-    # print("access []: [0]=" + shopping_name[0])   # ERROR:Traceback (most recent call last):IndexError: list index out of range
-
-    # print("access []: [-1]=" + shopping_name[-1]) # ERROR:Traceback (most recent call last):IndexError: list index out of range
+    # print(shopping_name[0])   # ERROR:Traceback (most recent call last):IndexError: list index out of range
+    # print(shopping_name[-1]) # ERROR:Traceback (most recent call last):IndexError: list index out of range
     return
 
 
@@ -84,11 +83,77 @@ def remove_item():
     return
 
 
+def sort_list():
+    sort_list_by_permanent_order()
+    sort_list_by_temporary_order()
+    return
+
+
+def sort_list_by_permanent_order():
+    # Permanent order
+    cars = ["bw", "ya", "abc", "ca"]
+    cars.sort()  # 正序排列，永久排序
+    print(cars)  # ['abc', 'bw', 'ca', 'ya']
+
+    print("\n")
+    languages = ['Java', 'C', "Python"]
+    languages.sort(reverse=True)  # 倒序排列，永久排序
+    print(languages)  # ['Python', 'Java', 'C']
+    return
+
+
+def sort_list_by_temporary_order():
+    nums = [1, 10, 5, 8]
+    num_temp = sorted(nums)  # 正序排列，临时排序
+    print(num_temp)  # [1, 5, 8, 10]
+    print(nums)  # [1, 10, 5, 8]
+
+    print("\n")
+    prices = [100, 5, 10]
+    prices_temp = sorted(prices, reverse=True)  # 倒序排列，临时排序
+    print(prices_temp)  # [100, 10, 5]
+    print(prices)  # [100, 5, 10]
+    return
+
+
+def reverse_list():
+    reverse_list_by_permanent_order()
+    reverse_list_by_temporary_order()
+
+
+def reverse_list_by_permanent_order():
+    stu = ["A", "C", "B"]
+    stu.reverse()  # 反转列表，not 倒序排列. 永久性修改，但通过再次调用恢复原来的排列顺序
+    print(stu)  # ['B', 'C', 'A']
+    stu.reverse()
+    print(stu)  # ['A', 'C', 'B']
+    return
+
+
+def reverse_list_by_temporary_order():
+    money = [5, 10, 8]
+    money_temp = list(reversed(money))  # 反转列表，not 倒序排列. 临时反转
+    # reversed(money) -> list_reverseiterator object at 0x1057fe450
+    print(money_temp)  # [8, 10, 5]
+    print(money)  # [5, 10, 8]
+    return
+
+
+def length_of_list():
+    names = ["A", "C"]
+    print(len(names))  # 2
+    print(len([]))  # 0
+    return
+
+
 def test():
-    # access_list_item()
+    access_list_item()
     # modify_item()
     # add_item()
-    remove_item()
+    # remove_item()
+    # sort_list()
+    # reverse_list()
+    # length_of_list()
     return
 
 
