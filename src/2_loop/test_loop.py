c7c328529a4524d3d9384a7eea6f9a50fde266d1
Vicky <ying.vicky.cao@outlook.com>
2019-12-05 13:45:41 +0800

    refactor

--- src/2_loop/test_loop.py
+++
@@ -1,9 +0,0 @@
-
-
-def test():
-    # traversing_list()
-    # create_value_list()
-    return
-
-
-test()
---
+++ src/3_loop/test_loop.py
@@ -0,0 +1,13 @@
+def traversing_list():
+    score = [90, 94, 98]
+    for item in score:
+        print(item)
+    return
+
+
+def test():
+    traversing_list()
+    return
+
+
+test()
